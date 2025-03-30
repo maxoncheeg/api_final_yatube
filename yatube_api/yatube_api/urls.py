526e2a8a6d34d314df6f57513d5d738f73477943
maxoncheeg <maksgigks@outlook.com>
2025-03-30 23:07:42 +0700

    add urls for future and add JWT authentication urls

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,6 +1,14 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+from yatube_api import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +18,11 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-]
-
+    path('api/v1/jwt/create/', TokenObtainPairView.as_view(),
+         name='jwt-create'),
+    path('api/v1/jwt/refresh/', TokenRefreshView.as_view(),
+         name='jwt-refresh'),
+    path('api/v1/jwt/verify/', TokenVerifyView.as_view(),
+         name='jwt-verify'),
+] + static(settings.MEDIA_URL,
+           document_root=settings.MEDIA_ROOT)

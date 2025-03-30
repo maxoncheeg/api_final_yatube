1eabbd1044d58bbdbd75afed5032621fa7766c49
maxoncheeg <maksgigks@outlook.com>
2025-03-30 23:26:30 +0700

    add views and urls

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,25 @@
-from django.urls import path
+from rest_framework import routers
+from django.urls import include, path
+
+from .views import PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet
+
+router = routers.DefaultRouter()
+
+router.register('posts', PostViewSet, basename='posts')
+router.register('groups', GroupViewSet, basename='groups')
+router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/posts/<int:post_id>/comments/',
+         CommentViewSet.as_view({
+             'get': 'list',
+             'post': 'create'}), name='comments-list'),
+    path('v1/posts/<int:post_id>/comments/<int:pk>/',
+         CommentViewSet.as_view({
+             'put': 'update',
+             'get': 'retrieve',
+             'patch': 'partial_update',
+             'delete': 'destroy'
+         }), name='comments-detail'),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,211 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import AllowAny
+from django_filters.rest_framework import DjangoFilterBackend
+
+from posts.models import Follow, Group, Post
+from .permissions import IsOwnerOrReadOnly
+from .serializers import (CommentSerializer, FollowSerializer,
+                          GroupSerializer, PostSerializer)
+
+
+class ViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    pagination_class = LimitOffsetPagination
+
+class PostViewSet(ViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.author != request.user:
+            return Response(
+                {"detail": "You do not "
+                            " have access"
+                            " to do" 
+                            " this action"}, status=status.HTTP_403_FORBIDDEN)
+        else:
+            serializer = self.get_serializer(instance, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if instance.author != request.user:
+            return Response(
+                {"detail": "You do not "
+                            " have access"
+                            " to do"
+                            " this action"}, status=status.HTTP_403_FORBIDDEN)
+
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, pk=post_id)
+        serializer.save(post=post, author=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+        try:
+            comment = Comment.objects.get(id=kwargs.get('pk'))
+            serializer = self.get_serializer(comment,
+                                             data=request.data,
+                                             partial=False)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data,
+                            status=status.HTTP_200_OK)
+        except Comment.DoesNotExist:
+            return Response({"detail": "Comment"
+                                       " cannot"
+                                       " be"
+                                       " found"}, 
+                                       status=status.HTTP_404_NOT_FOUND)
+    
+    def partial_update(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+
+            if instance.author != request.user:
+                return Response(
+                    {"detail": "You don't "
+                               " have enough"
+                               " rights to"
+                               " comment"}, status=status.HTTP_403_FORBIDDEN)
+
+            serializer = self.get_serializer(
+                instance,
+                data=request.data,
+                partial=True
+            )
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        except Http404:
+            return Response({"detail": "Comment"
+                                       " cannot"
+                                       " be"
+                                       " found"}, 
+                                       status=status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            if instance.author != request.user:
+                return Response({"detail": "You don't "
+                                           " have enough"
+                                           " rights to"
+                                           " perform"
+                                           " this action"},
+                                           status=status.HTTP_403_FORBIDDEN)
+            self.perform_destroy(instance)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Http404:
+            return Response({"detail": "Comment"
+                                       " cannot"
+                                       " be"
+                                       " found"}, 
+                                       status=status.HTTP_404_NOT_FOUND)
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        return Comment.objects.filter(post=post_id)
+
+    def retrieve(self, request, *args, **kwargs):
+        comment_id = self.kwargs.get('pk')
+        try:
+            comment = Comment.objects.get(id=comment_id)
+            serializer = CommentSerializer(comment)
+            return Response(serializer.data)
+        except Comment.DoesNotExist:
+            return Response({"detail": "Page not found."},
+                            status=status.HTTP_404_NOT_FOUND)
+
+
+class GroupViewSet(ViewSet):
+    serializer_class = GroupSerializer
+    queryset = Group.objects.all()
+    http_method_names = ['get', 'head', 'options']
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter]
+    search_fields = ['following__username']
+ 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED,
+            headers=headers
+        )
+
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            return Response({"detail":  "You don't "
+                                        " have enough"
+                                        " rights to"
+                                        " perform"
+                                        " this action"},
+                            status=status.HTTP_401_UNAUTHORIZED)
+
+        queryset = Follow.objects.filter(user=self.request.user)
+
+        search_query = self.request.query_params.get('search')
+        if search_query:
+            queryset = queryset.filter(
+                following__username__icontains=search_query
+            )
+        return queryset
+
+    def perform_create(self, serializer):
+        following_user = serializer.validated_data['following']
+
+        if following_user == self.request.user:
+            raise ValidationError(
+                {"detail": "You cannot be able to follow yourself"})
+
+        if Follow.objects.filter(
+                user=self.request.user,
+                following=following_user
+        ).exists():
+            raise ValidationError(
+                {"detail": "You already followed this user"})
+
+        serializer.save(user=self.request.user)
